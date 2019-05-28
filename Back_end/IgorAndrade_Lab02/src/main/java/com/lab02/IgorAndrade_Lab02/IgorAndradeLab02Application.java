e39bd3fa70c1b466c08b2c90487818afd57b5a3f
Igor Silveira <igor.andrade@ccc.ufcg.edu.br>
2019-05-27 22:10:46 -0300

    iniciado lab02

---
+++ Back_end/IgorAndrade_Lab02/src/main/java/com/lab02/IgorAndrade_Lab02/IgorAndradeLab02Application.java
@@ -0,0 +1,13 @@
+package com.lab02.IgorAndrade_Lab02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgorAndradeLab02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgorAndradeLab02Application.class, args);
+	}
+
+}
